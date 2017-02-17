737588ffcf545e7ee29eeab2731fa40a0b1a1cf7
MADREDIDDIO <teolinotrip@libero.it>
2017-02-17 17:45:20 +0100

    Update main.js

--- Client/main.js
+++ Client/main.js
@@ -1,32 +1,9 @@
-	var ctx = document.getElementById("ctx").getContext("2d");
-	ctx.font = '30px Arial';
+var ctx = document.getElementById("ctx").getContext("2d"),
 
-	var socket = io();
+		socket = io();
 
-	socket.on('newPositions',function(data){
-		ctx.clearRect(0,0,500,500);
-		for(var i = 0 ; i < data.length; i++)
-			ctx.fillRect(data[i].x-5,data[i].y-5,10,10);		
-	});
-
-	document.onkeydown = function(event){
-		if(event.keyCode === 68)	//d
-			socket.emit('keyPress',{inputId:'right',state:true});
-		else if(event.keyCode === 83)	//s
-			socket.emit('keyPress',{inputId:'down',state:true});
-		else if(event.keyCode === 65) //a
-			socket.emit('keyPress',{inputId:'left',state:true});
-		else if(event.keyCode === 87) // w
-			socket.emit('keyPress',{inputId:'up',state:true});
-
-	}
-	document.onkeyup = function(event){
-		if(event.keyCode === 68)	//d
-			socket.emit('keyPress',{inputId:'right',state:false});
-		else if(event.keyCode === 83)	//s
-			socket.emit('keyPress',{inputId:'down',state:false});
-		else if(event.keyCode === 65) //a
-			socket.emit('keyPress',{inputId:'left',state:false});
-		else if(event.keyCode === 87) // w
-			socket.emit('keyPress',{inputId:'up',state:false});
-	}
+socket.on('newPositions',function(data){
+	ctx.clearRect(0,0,500,500);
+	for(var i = 0 ; i < data.length; i++)
+	ctx.fillRect(data[i].x-5,data[i].y-5,10,10);		
+});
