19e95e098da3e373f2f0a6ea34b7a169749c4279
MADREDIDDIO <teolinotrip@libero.it>
2017-02-17 17:46:13 +0100

    Update main.js

--- Client/main.js
+++ Client/main.js
@@ -1,9 +1,9 @@
 var ctx = document.getElementById("ctx").getContext("2d"),
 
-		socket = io();
+    socket = io();
 
 socket.on('newPositions',function(data){
-	ctx.clearRect(0,0,500,500);
-	for(var i = 0 ; i < data.length; i++)
-	ctx.fillRect(data[i].x-5,data[i].y-5,10,10);		
+  ctx.clearRect(0,0,500,500);
+  for(var i = 0 ; i < data.length; i++)
+  ctx.fillRect(data[i].x-5,data[i].y-5,10,10);		
 });
