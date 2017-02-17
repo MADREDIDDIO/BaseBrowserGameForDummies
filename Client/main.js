49ce2d93b9c837e9c8e4c092b21d01f47554c022
MADREDIDDIO <teolinotrip@libero.it>
2017-02-17 18:16:41 +0100

    Update main.js

--- Client/main.js
+++ Client/main.js
@@ -1,13 +1,19 @@
+//Impongo dei parametri di controllo più rigidi sulla sintassi del linguaggio
+"use strict";
+    //Acquisisco il contesto
 var ctx = document.getElementById("ctx").getContext("2d"),
-
+    //instauro un canele di comunicazione con il server
     socket = io(),
-    
+    //Futuro vettore di oggetti di tipo Player in gioco
     playerInGame = null;
-
+//Quando viene inviato l'aggiornamento dei giocatori in gioco dal server..
 socket.on('uploadPlayers',function(data){
+  //.. i dati vengono processati..
+    
+  //.. e viene aggiornato il vettore contenente gli oggetti di tipo Player
   playersInGame = data;
 });
-
+//Quando la finestra del browser è carica inizia il loop di gioco
 window.onload(function mainLoop(){
   ctx.clearRect(0,0,500,500);
   for(var i = 0 ; i < playerInGame.length; i++)
