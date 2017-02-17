45a230250023db5e124f6a0d5e7759805fc9411f
MADREDIDDIO <teolinotrip@libero.it>
2017-02-17 18:59:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,58 +3,33 @@ var app = express();
 var serv = require('http').Server(app);
 var ID = 0;
 
-
 app.get('/',function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
 
 serv.listen(process.env.PORT || 2000);
+
+var io = require('socket.io')(serv,{});
+
 console.log("Server started.");
 
 var SOCKET_LIST = {};
 var PLAYER_LIST = {};
 
-/*var Player = function(id){
-	var self = {
-		x:250,
-		y:250,
-		id:id,
-		number:"" + Math.floor(10 * Math.random()),
-		pressingRight:false,
-		pressingLeft:false,
-		pressingUp:false,
-		pressingDown:false,
-		maxSpd:10,
-	}
-	self.updatePosition = function(){
-		if(self.pressingRight)
-			self.x += self.maxSpd;
-		if(self.pressingLeft)
-			self.x -= self.maxSpd;
-		if(self.pressingUp)
-			self.y -= self.maxSpd;
-		if(self.pressingDown)
-			self.y += self.maxSpd;
-	}
-	return self;
-}*/
-
 //Constructor for Player_Entity_Type
-function Player(id){
+function Player(id,x,y){
 
-	this.x  = 250;
-	this.y  = 250;
+	this.x  = x || 250;
+	this.y  = y || 250;
 	this.id = id;
-	this.num= "" + Math.floor(10 * Math.random());//stringa con numero da 0 a 9
+	this.spd= 10;
 	
 };
 
-var io = require('socket.io')(serv,{});
-
 io.sockets.on('connection', function(socket){
   
-  socket.id = ++ID;//Math.random();due id potrebbero essere uguali, inoltre è un valore float, molto meglio lavorare con int dove possibile. In questi casi consigliano o contatore o Symbol()
+  socket.id = ++ID;
 	SOCKET_LIST[socket.id] = socket;
 
 	var player = new Player(socket.id);
